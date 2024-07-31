3eb8081a0aa38356443731a8e63291734089fd07
williamweatherholtz <williamweatherholtz@gmail.com>
2024-07-31 00:52:04 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Weatherholtz Family Videos",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    baseUrl: "famvids.willweatherholtz.com",
+    ignorePatterns: ["private", "templates", ".obsidian", "classes"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
